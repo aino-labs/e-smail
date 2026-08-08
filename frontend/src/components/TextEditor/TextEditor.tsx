d235c1bc3f64db0e55169d9cd045a7457405a1cb
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2026-08-08 22:00:50 +0300

    feat: links are detected automatically

--- frontend/src/components/TextEditor/TextEditor.tsx
+++ frontend/src/components/TextEditor/TextEditor.tsx
@@ -37,7 +37,65 @@ export default function TextEditor({
       Underline,
       Link.configure({
         openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+        protocols: ["http", "https"],
         HTMLAttributes: { target: "_blank", rel: "noopener noreferrer" },
+        isAllowedUri: (url, ctx) => {
+          try {
+            const parsedUrl = url.includes(":")
+              ? new URL(url)
+              : new URL(`${ctx.defaultProtocol}://${url}`);
+
+            if (!ctx.defaultValidate(parsedUrl.href)) {
+              return false;
+            }
+
+            const disallowedProtocols = ["ftp", "file", "mailto"];
+            const protocol = parsedUrl.protocol.replace(":", "");
+
+            if (disallowedProtocols.includes(protocol)) {
+              return false;
+            }
+
+            const allowedProtocols = ctx.protocols.map((p) =>
+              typeof p === "string" ? p : p.scheme,
+            );
+
+            if (!allowedProtocols.includes(protocol)) {
+              return false;
+            }
+
+            // const disallowedDomains = [
+            //   "example-phishing.com",
+            //   "malicious-site.net",
+            // ];
+            // const domain = parsedUrl.hostname;
+
+            // if (disallowedDomains.includes(domain)) {
+            //   return false;
+            // }
+
+            return true;
+          } catch {
+            return false;
+          }
+        },
+        shouldAutoLink: (url) => {
+          try {
+            // construct URL
+            const parsedUrl = url.includes(":")
+              ? new URL(url)
+              : new URL(`https://${url}`);
+
+            const disallowedDomains: string[] = [];
+            const domain = parsedUrl.hostname;
+
+            return !disallowedDomains.includes(domain);
+          } catch {
+            return false;
+          }
+        },
       }),
       Placeholder.configure({
         placeholder: t("body_placeholder"),
